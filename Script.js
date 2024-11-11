1ac93bdaeb4371ff71b66ab87dd16471670042f9
sankar360 <sivaram1712000@gmail.com>
2024-11-11 20:32:40 +0530

    new file added

---
+++ Script.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+    $('#signup').validate(
+       {
+        rules:{
+            fname:{
+                required:true,
+                minlength:5,
+            },
+            lname:{
+                required:true,
+                minlength:5,
+            },
+            mobile:{
+                required:true,
+                minlength:10,
+            },
+            password:{
+                required:true,
+                minlength:8,
+            },
+            day:{
+                required:true,
+            },
+            month:{
+                required:true,
+            },
+            year:{
+                required:true,
+            },
+            Gender:{
+                required:true,
+            }
+
+        },
+        messages:{
+            fname:{
+                required:"please enter your firstname",
+            } 
+        }
+    }) 
+})
